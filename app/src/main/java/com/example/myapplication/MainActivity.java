78b47aafed87452ccae31eaa494fa34470a4a2ba
mkbzas777 <835619974@qq.com>
2021-04-26 21:59:00 +0800

    SearchHistory实体类的建立与Litepal导入以及SearchHistoryDao数据库操作方法的完成

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity" ;
     private Button btn1,btn2;
---
+++ app/src/main/java/com/example/myapplication/util/SearchHistoryDao.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.util;
+
+import android.util.Log;
+
+import com.example.myapplication.entity.SearchHistory;
+
+import org.litepal.LitePal;
+
+import java.util.ArrayList;
+
+public class SearchHistoryDao {
+    private static final String TAG = "SearchHistoryDao";
+
+    public SearchHistoryDao() {
+    }
+
+    public static ArrayList<SearchHistory> findAll(){
+        ArrayList<SearchHistory> arrayList = new ArrayList<>();
+        arrayList.addAll(LitePal.findAll(SearchHistory.class));
+        return arrayList;
+    }
+
+    public static SearchHistory find(Integer id){
+        Log.d(TAG, "find: "+LitePal.find(SearchHistory.class,id));
+        return LitePal.find(SearchHistory.class,id);
+    }
+
+    public static void insert(SearchHistory SearchHistory){
+        SearchHistory.save();
+        Log.d(TAG, "insert: "+SearchHistory.toString());
+    }
+
+    public static void delete(Integer id){
+        Log.d(TAG, "delete: "+LitePal.find(SearchHistory.class,id));
+        LitePal.delete(SearchHistory.class,id);
+    }
+
+    public static void deleteAll(){
+        Log.d(TAG, "deleteAll");
+        LitePal.deleteAll(SearchHistory.class);
+    }
+
+    public static void update(SearchHistory SearchHistory){
+        String min = LitePal.min( SearchHistory.class,"searchTime",String.class);
+        Log.d(TAG, "update from: "+LitePal.where("searchTime = ?",min).find(SearchHistory.class));
+        SearchHistory.updateAll("searchTime = ?",min);
+        String max = LitePal.max( SearchHistory.class,"searchTime",String.class);
+        Log.d(TAG, "update to: "+LitePal.where("searchTime = ?",max).find(SearchHistory.class));
+    }
+
+}
